002548d47cc5da8d31fc27a381b8c60dce5177f2
varshitjain01 <143643196+varshitjain01@users.noreply.github.com>
2025-10-24 05:40:09 -0700

    Added link to instagram button

--- frontend/src/components/Layout/Topbar.jsx
+++ frontend/src/components/Layout/Topbar.jsx
@@ -8,13 +8,13 @@ const Topbar = () => {
     <div className="bg-rabbit-red text-white flex">
         <div className="container mx-auto flex justify-between item-center py-3 px-4"> 
             <div className=' hidden md:flex items-center space-x-4'>
-                <a href="#" className="hover:text-gray-300">
+                <a href="https://www.instagram.com/imnot.varshitt?igsh=eW52bGxlcHE1aDZ2&utm_source=qr" className="hover:text-gray-300">
                     <TbBrandMeta className="h-5 w-5"/>
                 </a>
-                 <a href="#" className="hover:text-gray-300">
+                 <a href="https://www.instagram.com/imnot.varshitt?igsh=eW52bGxlcHE1aDZ2&utm_source=qr" className="hover:text-gray-300">
                     <IoLogoInstagram className="h-5 w-5"/>
                 </a>
-                 <a href="#" className="hover:text-gray-300">
+                 <a href="https://www.instagram.com/imnot.varshitt?igsh=eW52bGxlcHE1aDZ2&utm_source=qr" className="hover:text-gray-300">
                     <RiTwitterXLine className="h-4 w-4"/>
                 </a>
             </div> 
